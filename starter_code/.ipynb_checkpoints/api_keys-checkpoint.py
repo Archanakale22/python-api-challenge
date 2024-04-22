050c600d28ff4f5ca794674d11ad6f48df04cc79
Archanakale22 <archana.kale.9@gmail.com>
2024-04-22 22:53:37 +1000

    commit

---
+++ starter_code/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "34019506a14f335ceb1a6eb9d2198750"
+
+# Geoapify API Key
+geoapify_api_key = "166bc8b0db244610a9262921fd316232"
+
---
+++ starter_code/api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "My_api_key"
+
+# Geoapify API Key
+geoapify_api_key = "My_api_key"
+
+# GMap API Key
+gmap_api_key = "My_api_key"
+
